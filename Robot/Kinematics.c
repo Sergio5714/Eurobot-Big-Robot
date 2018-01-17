4eb062b54e6082f059faa78410206d09fa9ab9c5
Sergio5714 <sergey57145714@mail.ru>
2018-01-17 08:56:03 +0300

    Mistake in inverse and forward kinematics matrices was fixed

--- Robot/Kinematics.c
+++ Robot/Kinematics.c
@@ -5,22 +5,22 @@
 
 // Note : matrix of forward kinematics already takes into account 2 cylindrical transmissions (wheels 2 and 4)
 
-float MLineSpeed[4][3] = { 47.14,  -47.14,  0.0,  // forward linear matrix
-                           47.14,   47.14,  0.0,
-                           47.14,   47.14,  0.0,
-                           47.14,  -47.14,  0.0};
+float MLineSpeed[4][3] = { 23.57022667,  -23.57022667,  0.0,  // forward linear matrix
+                           23.57022667,   23.57022667,  0.0,
+                           23.57022667,   23.57022667,  0.0,
+                           23.57022667,  -23.57022667,  0.0};
 
 
-float MRotSpeed[4][3] = { 0.0,  0.0,  -6.85,  // forward rotation matrix
-                          0.0,  0.0,   6.85,
-                          0.0,  0.0,  -6.85,
-                          0.0,  0.0,   6.85};
+float MRotSpeed[4][3] = { 0.0,  0.0,  -5.5485487,  // forward rotation matrix
+                          0.0,  0.0,   5.5485487,
+                          0.0,  0.0,  -5.5485487,
+                          0.0,  0.0,   5.5485487};
 
 
 //For the third picture in the solution part
-float InverseKinematics[3][4] = { 0.0053025, -0.0053025,   0.0053025,  -0.0053025,  // inverse matrix
-                                 -0.0053025, -0.0053025,   0.0053025,   0.0053025,     
-                                 -0.0362425, -0.0362425,  -0.0362425,  -0.0362425 };
+float InverseKinematics[3][4] = { 0.0106066, -0.0106066,   0.0106066,  -0.0106066,  // inverse matrix
+                                 -0.0106066, -0.0106066,   0.0106066,   0.0106066,     
+                                 -0.04505683, -0.04505683,  -0.04505683,  -0.04505683 };
 
 
 
